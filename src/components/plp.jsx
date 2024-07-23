a2b2ef33f500fee1de5622a7d0200c348e132aac
millenniumSonule <anshumansonuley@gmail.com>
2024-07-23 14:06:46 +0530

    .

--- src/components/plp.jsx
+++ src/components/plp.jsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from './navbar'
 
-const plp = () => {
+const Plp = () => {
+    
+  const [count, setCount] = useState(0);
+
+  const Incriment = () => {
+    setCount(count+1);
+  }
+
+  const Decrement = () => {
+    setCount(count-1);
+  }
+
   return (
     <div className="plp-container">
       <Navbar/>
-        
+        <button onClick={Incriment}>Incriment</button>
+        <p>{count}</p>
+        <button onClick={Decrement}>Decriment</button>
     </div>
   )
 }
 
-export default plp
+export default Plp
