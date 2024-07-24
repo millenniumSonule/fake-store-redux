c493e196ca6bc3b4343412b594ad09b507248a5e
millenniumSonule <anshumansonuley@gmail.com>
2024-07-24 12:49:33 +0530

    ,

--- src/components/plp.jsx
+++ src/components/plp.jsx
@@ -1,18 +1,13 @@
 import React, { useState } from 'react';
 import Navbar from './navbar'
-import { increment, decrement } from '../actions/counterSlice';
-import { useSelector, useDispatch } from 'react-redux';
 
 const Plp = () => {
     
-  const count = useSelector((c) => c.counter.value);
 
   return (
     <div className="plp-container">
       <Navbar/>
-        <button onClick={useDispatch(increment())}>Incriment</button>
-        <p>{count}</p>
-        <button onClick={useDispatch(decrement())}>Decriment</button>
+
     </div>
   )
 }
--- src/store/store.jsx
+++ src/store/store.jsx
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import thunk from 'redux-thunk';
 
 
 export const store = configureStore({
